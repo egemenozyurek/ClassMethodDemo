452d7cde742e4892cc3e591160fc49e019147022
egemenozyurek <ozyurekegemen95@gmail.com>
2021-01-18 16:25:38 +0300

    first commit

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+            }
+        }
+
+        public void musteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void musteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + "In Loving Memory");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.Ad = "Egemen";
+            musteri.Soyad = "Ozyurek";
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 2;
+            musteri1.Ad = "James";
+            musteri1.Soyad = "Hetfield";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 3;
+            musteri2.Ad = "Chester";
+            musteri2.Soyad = "Bennington";
+
+            Musteri[] musteriler = new Musteri[] { musteri, musteri1, musteri2 };
+
+            MusteriManager legends = new MusteriManager();
+
+            Console.WriteLine("***************Ekleme**************");
+            legends.musteriEkle(musteri);
+            legends.musteriEkle(musteri1);
+            legends.musteriEkle(musteri2);
+
+            Console.WriteLine("***************Listeleme**************");
+            legends.MusteriListele(musteriler);
+
+            Console.WriteLine("***************Silme**************");
+            legends.musteriSil(musteri2);
+        }
+    }
+}
